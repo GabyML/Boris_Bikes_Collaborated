120eecaee423a94b4c5826dca859df37742b9967
Aftermaths <fnzzhou@gmail.com>
2015-07-23 16:53:28 +0100

    post challenge 2 started

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,13 +1,27 @@
 require_relative './bike'
 
 class DockingStation
+  def initialize
+    @bike = []
+  end
 
-	def release_bike
-		fail "No bikes - disaster!" unless @bike
-		@bike
-	end
+  def release_bike
+    fail 'No bikes - disaster!' if empty?
+    @bike.pop
+  end
 
-	def dock (bike)
-		@bike = bike
-	end
+  def dock(bike)
+    fail 'Station is full!' if full?
+    @bike << bike
+  end
+
+  private
+
+  def empty?
+    @bike.empty?
+  end
+
+  def full?
+    @bike.length == 20
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,13 +5,22 @@ describe DockingStation do
 
 	it "releases a working bike" do
 		subject.dock Bike.new
-		expect(subject.release_bike).to be_working
+    bike = subject.release_bike
+		expect(bike).to be_working
 	end
 
-	it "raises an error if there are no bikes available" do
-		expect{subject.release_bike}.to raise_error("No bikes - disaster!")
-	end
+  describe "#release_bike" do
+    it "raises an error if there are no bikes available" do
+    expect{subject.release_bike}.to raise_error("No bikes - disaster!")
+    end
+  end
 
 	it { is_expected.to respond_to(:dock).with(1).argument }
 
+  describe '#dock' do
+    it 'raises an error if station is full' do
+      20.times { subject.dock Bike.new }
+      expect { subject.dock Bike.new }.to raise_error('Station is full!')
+    end
+  end
 end
